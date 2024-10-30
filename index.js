774674ba9672ad707fa642b3e46070cff3c9fb17
Zhenya Datsok <yevheniia.datsok@sytoss.com>
2024-10-30 21:17:14 +0100

    Lab3_JS - Implemented some tasks

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-// This script listens for the scroll event on the window
-let headerEl = document.getElementById("header")
+// index.js
 
-window.addEventListener("scroll", function() {
-    const scrollPos = window.scrollY
+// Get elements
+let headerEl = document.getElementById("header");
+let showMenu = document.getElementById("show-menu");
+let hiddenMenu = document.getElementById("hidden-menu");
+let closeButton = document.querySelector(".close");
+let scrollToTopButton = document.getElementById("scroll-to-top");
+
+// Scroll handling
+window.addEventListener("scroll", function () {
+    const scrollPos = window.scrollY;
     if (scrollPos > 1) {
-        headerEl.classList.add("fixed")
+        headerEl.classList.add("fixed");
+        scrollToTopButton.style.display = 'block'; // Show scroll-to-top button
     } else {
-        headerEl.classList.remove("fixed")
+        headerEl.classList.remove("fixed");
+        scrollToTopButton.style.display = 'none'; // Hide scroll-to-top button
     }
-})
+});
+
+// Handler for burger menu button
+showMenu.addEventListener("click", function () {
+    hiddenMenu.classList.add("show");
+});
+
+// Handler for menu close button
+closeButton.addEventListener("click", function () {
+    hiddenMenu.classList.remove("show");
+});
+
+// Handler for scroll-to-top button
+scrollToTopButton.addEventListener("click", function () {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+});
